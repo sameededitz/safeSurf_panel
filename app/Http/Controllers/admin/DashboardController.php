cb179a61a45550e4df58db94d92841e634529ef7
Sameed <sameedhassan22@gmail.com>
2025-05-21 18:44:39 +0500

    feat: Refactor SMTP logging to use DashboardController method

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Traits\UsesDynamicSmtp;
 
 class DashboardController extends Controller
 {
+    use UsesDynamicSmtp;
+
     public function index()
     {
         return view('admin.dashboard');
+    }
+
+    public function logSmtp()
+    {
+        $this->applySmtpConfig();
+        \Illuminate\Support\Facades\Log::info('SMTP log test', [
+            'host' => config('mail.mailers.smtp.host'),
+            'port' => config('mail.mailers.smtp.port'),
+            'username' => config('mail.mailers.smtp.username'),
+            'password' => config('mail.mailers.smtp.password'),
+            'from' => config('mail.from.address'),
+            'from_name' => config('mail.from.name'),
+        ]);
+        return 'Log test completed';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,14 +15,4 @@ Route::get('/', function () {
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
 
-Route::get('/log-smtp', function () {
-    \Illuminate\Support\Facades\Log::info('SMTP log test', [
-        'host' => config('mail.mailers.smtp.host'),
-        'port' => config('mail.mailers.smtp.port'),
-        'username' => config('mail.mailers.smtp.username'),
-        'password' => config('mail.mailers.smtp.password'),
-        'from' => config('mail.from.address'),
-        'from_name' => config('mail.from.name'),
-    ]);
-    return 'Log test completed';
-})->name('log-smtp');
+Route::get('/log-smtp', [DashboardController::class, 'logSmtp'])->name('log-smtp');
