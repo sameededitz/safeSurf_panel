897b6b2ab3e1eeed08020652d01f4bfcf16aff65
Sameed <sameedhassan22@gmail.com>
2025-05-21 18:42:31 +0500

    feat: Add SMTP logging route for testing mail configuration

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     if (Auth::check()) {
         return redirect()->route('admin.dashboard');
@@ -16,6 +14,14 @@ Route::get('/', function () {
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
 
-Route::get('/test', function () {
-    return view('auth.verify');
-});
+Route::get('/log-smtp', function () {
+    \Illuminate\Support\Facades\Log::info('SMTP log test', [
+        'host' => config('mail.mailers.smtp.host'),
+        'port' => config('mail.mailers.smtp.port'),
+        'username' => config('mail.mailers.smtp.username'),
+        'password' => config('mail.mailers.smtp.password'),
+        'from' => config('mail.from.address'),
+        'from_name' => config('mail.from.name'),
+    ]);
+    return 'Log test completed';
+})->name('log-smtp');
