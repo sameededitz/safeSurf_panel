3231c822d1826d53b26f7264da6b1deb4eea8064
Azil6744 <developmentwithazil@gmail.com>
2025-06-19 13:50:19 +0500

    feat: Add email verification functionality with VerifyController

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VerifyController;
 use App\Livewire\Actions\Logout;
 use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', Login::class)->name('login')->middleware('guest');
 
 Route::post('/logout', Logout::class)->name('logout')->middleware('auth');
+Route::get('/email/verify/{id}/{hash}', [VerifyController::class, 'verify'])->middleware(['signed'])->name('verification.verify');
